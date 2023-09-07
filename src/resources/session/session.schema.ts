dac9afba397b5ba8aae5ad41059c1ba736b155f6
Damien Butt <damien.butt@norgateav.com>
2023-09-07 23:32:58 +0100

    refactor: ensure email and password are not empty

--- src/resources/session/session.schema.ts
+++ src/resources/session/session.schema.ts
@@ -2,12 +2,16 @@ import { TypeOf, z } from "zod";
 
 export const createSessionSchema = z.object({
     body: z.object({
-        email: z.string({
-            required_error: "Email is required",
-        }),
-        password: z.string({
-            required_error: "Password is required",
-        }),
+        email: z
+            .string({
+                required_error: "Email is required",
+            })
+            .nonempty(),
+        password: z
+            .string({
+                required_error: "Password is required",
+            })
+            .nonempty(),
     }),
 });
 
